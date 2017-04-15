37ac6675eeaa0f5882e760f513d7355d5c4ad457
NatalishaKaf <natalya-strel@yandex.ru>
2017-04-16 02:31:57 +0400

    Создание класса

--- Sandbox/src/main/java/ru/dis/points/Point.java
+++ Sandbox/src/main/java/ru/dis/points/Point.java
@@ -3,17 +3,18 @@ package ru.dis.points;
 
 public class Point {
   public static void main(String[] args){
+    DistanceBetweenPoints dbp= new DistanceBetweenPoints;
     double x1=2;
     double x2= 5;
     double y1= 1;
     double y2= 7;
-    double p1= x2-x1;
-    double p2= y2-y1;
+    dbp.p1= x2-x1;
+    dbp.p2= y2-y1;
 
-    System.out.println("Расстояние между двумя точками " + p1 + " и " + p2 + " на плоскости = " + distance(p1,p2));
+    System.out.println("Расстояние между двумя точками " + dbp.p1 + " и " + dbp.p2 + " на плоскости = " + distance(dbp));
   }
 
-  public static double distance(double p1, double p2) {
-    return Math.sqrt(p1*p1+p2*p2);
+  public static double distance(DistanceBetweenPoints dbp) {
+    return Math.sqrt(dbp.p1* dbp.p1+dbp.p2* dbp.p2);
   }
 }
