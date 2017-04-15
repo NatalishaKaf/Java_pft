10029b185380e8a2dff11e9d1237bbd9e39c4324
NatalishaKaf <natalya-strel@yandex.ru>
2017-04-16 02:47:00 +0400

    Добавлен класс

---
+++ Sandbox/src/main/java/ru/dis/points/DistanceBetweenPoints.java
@@ -0,0 +1,17 @@
+package ru.dis.points;
+
+
+public class DistanceBetweenPoints {
+
+  public double x1;
+  public double x2;
+  public double y1;
+  public double y2;
+
+  public DistanceBetweenPoints (double x1, double x2, double y1, double y2) {
+    this.x1=x1;
+    this.x2=x2;
+    this.y1=y1;
+    this.y2=y2;
+  }
+}
