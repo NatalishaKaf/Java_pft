724ad984f1c9b1404eb82b9e5991768d059f0f6f
NatalishaKaf <natalya-strel@yandex.ru>
2017-04-16 01:55:25 +0400

    Создание функции вычисления расстояния между двумя точками

--- Sandbox/src/main/java/ru/dis/points/Point.java
+++ Sandbox/src/main/java/ru/dis/points/Point.java
@@ -9,6 +9,11 @@ public class Point {
     double y2= 7;
     double p1= x2-x1;
     double p2= y2-y1;
-    System.out.println("Расстояние между двумя точками" + p1 + "и " + p2 + " на плоскости = " + Math.sqrt((p1*p1)+(p2*p2)));
+
+    System.out.println("Расстояние между двумя точками " + p1 + " и " + p2 + " на плоскости = " + distance(p1,p2));
+  }
+
+  public static double distance(double p1, double p2) {
+    return Math.sqrt(p1*p1+p2*p2);
   }
 }
