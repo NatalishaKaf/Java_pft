88b452d76c858086ee08fab2c2e8b396fa0974ff
NatalishaKaf <natalya-strel@yandex.ru>
2017-04-20 21:49:43 +0300

    Main.java

--- Sandbox/src/main/java/ru/dis/points/Main.java
+++ Sandbox/src/main/java/ru/dis/points/Main.java
@@ -1,13 +1,11 @@
 package ru.dis.points;
 
-/**
- * Created by jonik on 18.04.2017.
- */
+
 public class Main {
   public static void main(String[] args) {
-    Point p1 = new Point(2,4);
-    Point p2 = new Point(9,5);
-    System.out.println("Расcтояние между двумя точками на плоскости= " + p1.distance(p1,p2));
+    Point p1 = new Point(0,5);
+    Point p2 = new Point(15,7);
+    System.out.println("Расcтояние между двумя точками на плоскости= " + p1.distance(p2));
   }
 
 }
