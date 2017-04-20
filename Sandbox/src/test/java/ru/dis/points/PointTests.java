90503031c491344bc1b633354eca4af0d9a75d2a
NatalishaKaf <natalya-strel@yandex.ru>
2017-04-20 22:39:06 +0400

    Sandbox

---
+++ Sandbox/src/test/java/ru/dis/points/PointTests.java
@@ -0,0 +1,15 @@
+package ru.dis.points;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+  @Test
+  public void testDistance (){
+Point p1= new Point(2, 4);
+Point p2= new Point (5,8);
+    Assert.assertEquals(p1.distance(p2), 5.0);
+  }
+}
