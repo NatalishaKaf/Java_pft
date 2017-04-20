a493ee12010b4e4aacbe00f492015522e8189534
NatalishaKaf <natalya-strel@yandex.ru>
2017-04-20 21:51:07 +0300

    Point.java

--- Sandbox/src/main/java/ru/dis/points/Point.java
+++ Sandbox/src/main/java/ru/dis/points/Point.java
@@ -9,9 +9,7 @@ public class Point {
     this.x = x;
     this.y = y;
   }
-  public double distance(Point Point1, Point Point2) {
-    return Math.sqrt((Point1.x - Point2.x)*(Point1.x - Point2.x) + (Point1.y - Point2.y)*(Point1.y - Point2.y));
+  public double distance(Point Point2) {
+    return Math.sqrt((x - Point2.x)*(x - Point2.x) + (y - Point2.y)*(y - Point2.y));
   }
   }
-
-
