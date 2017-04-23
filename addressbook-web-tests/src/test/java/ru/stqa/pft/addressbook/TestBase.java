831153ad851423e6bb60b56bba928170fd1fcac7
NatalishaKaf <natalya-strel@yandex.ru>
2017-04-23 21:20:13 +0400

    Добавление методов из группы в TestBase из Contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -66,6 +66,48 @@ public class TestBase {
     wd.findElement(By.linkText("groups")).click();
   }
 
+  protected void ReturnHomePage() {
+    wd.findElement(By.xpath("//div/div[4]/div/i/a[2]")).click();
+  }
+
+  protected void SubmitContactCreation() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  protected void fillFormContact(String firstname, String middlename, String lastname, String nickname, String title, String company, String address, String home, String email) {
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(firstname);
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys(middlename);
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(lastname);
+    wd.findElement(By.name("nickname")).click();
+    wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys(nickname);
+    wd.findElement(By.name("title")).click();
+    wd.findElement(By.name("title")).clear();
+    wd.findElement(By.name("title")).sendKeys(title);
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(company);
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(address);
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(home);
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(email);
+
+  }
+
+  protected void goToNewFormContact() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
   @AfterMethod
     public void tearDown() {
         wd.quit();
@@ -76,6 +118,23 @@ public class TestBase {
   }
 
   protected void SelectGroup() {
+      wd.findElement(By.name("selected[]")).click();
+  }
+
+  protected void goToHome() {
+      wd.findElement(By.linkText("home")).click();
+  }
+
+  protected void goToAlert() {
+      wd.switchTo().alert().accept();
+  }
+
+  protected void submitDeleteContact() {
+      wd.findElement(By.xpath("//div[@id='content']/form[2]/div[2]/input")).click();
+  }
+
+  protected void goToFormContacts() {
+      wd.findElement(By.linkText("home")).click();
       wd.findElement(By.name("selected[]")).click();
   }
 }
