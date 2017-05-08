a251e5b709ace2e95d792b6f4db338e376bf1935
NatalishaKaf <natalya-strel@yandex.ru>
2017-05-09 02:41:31 +0400

    Запуск тестов с Mozilla FF, GChrome, IExplorer

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,21 +1,34 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import sun.plugin2.util.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-  FirefoxDriver wd;
+  private WebDriver wd;
 
   private SessionHelper sessionHelper;
-  private  NavigationHelper navigationHelper;
+  private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
+  private String browser;
 
-
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser == org.openqa.selenium.remote.BrowserType.FIREFOX) {
+      wd = new FirefoxDriver();
+    } else if (browser == org.openqa.selenium.remote.BrowserType.IEXPLORE) {
+      wd = new InternetExplorerDriver();
+    } else if (browser == org.openqa.selenium.remote.BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    }
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
@@ -23,8 +36,6 @@ public class ApplicationManager {
     sessionHelper = new SessionHelper(wd);
     sessionHelper.Login("admin", "secret");
   }
-
-
 
   public void Stop() {
     wd.quit();
