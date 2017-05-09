6214781d3a0b07398a9ad1c2b08f03ce36f7bd94
NatalishaKaf <natalya-strel@yandex.ru>
2017-05-09 19:25:55 +0400

    Дефолтные значения в Создании Контактов и Групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -7,7 +7,7 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void GroupObjectsCreationTestContacts() {
     app.getGroupHelper().goToNewFormContact();
-    app.getGroupHelper().fillFormContact("fgdfg", "xfdgfg", "xcv", "dfg", "dxfbcdvb", "cvbvcb", "address", "sdfsdf12355", "");
+    app.getGroupHelper().fillFormContact("fgdfg", "xfdgfg", "xcv", "dfg", null, "cvbvcb", "address", "sdfsdf12355", null);
     app.getGroupHelper().SubmitContactCreation();
     app.getGroupHelper().ReturnHomePage();
   }
