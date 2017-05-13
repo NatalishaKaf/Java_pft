0fc7a1db4f6d15930ace23ccb0e945ab8427235c
NatalishaKaf <natalya-strel@yandex.ru>
2017-05-13 23:42:48 +0300

    Создание лямда-выражений

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,10 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
 import com.sun.xml.internal.messaging.saaj.packaging.mime.util.LineInputStream;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactObjects;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -26,7 +28,9 @@ public class ContactCreationTests extends TestBase {
         max= g.getId();
       }
     }
-    contact.setId(max);
+    //Comparator<? super ContactObjects> ById= (Comparator<ContactObjects>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+    //int max1= after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals (new HashSet<Object>(before), new HashSet<Object>(after));
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupObjects;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase{
@@ -12,22 +14,23 @@ public class GroupCreationTests extends TestBase{
   public void testGroupCreation() {
     app.getNavigationHelper().goToGroupPage();
     List<GroupObjects> before = app.getGroupHelper().getGroupList();
-    GroupObjects group= new GroupObjects("test1",null, null);
+    GroupObjects group= new GroupObjects("test8",null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupObjects> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(),before.size()+1);
 
 
-    int max= 0;
-    for(GroupObjects g:after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    after.stream()
-    group.setId (max);
+   // int max= 0;
+    //for(GroupObjects g:after) {
+     // if (g.getId() > max) {
+       // max = g.getId();
+      //}
+    //}
+    //Comparator<? super GroupObjects> byId= (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+  //  int max1 = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+    group.setId (after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
-    Assert.assertEquals(after.size(),before.size());
+    Assert.assertEquals (new HashSet<Object>(before), new HashSet<Object>(after));
     }
 
   }
