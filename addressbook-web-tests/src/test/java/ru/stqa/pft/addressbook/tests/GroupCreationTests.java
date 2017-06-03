0809b3f95ddb892e015444d65fa7c4072e8f9fb3
NatalishaKaf <natalya-strel@yandex.ru>
2017-06-03 19:05:37 +0300

    Создание лог-файла

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -2,6 +2,8 @@ package ru.stqa.pft.addressbook.tests;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactObjects;
@@ -49,6 +51,7 @@ public class GroupCreationTests extends TestBase {
             Groups after = app.group().all();
             assertThat(after, equalTo
                     (before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
         }
 
 
